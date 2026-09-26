6cbb026e778a6ceda89c4f5fc8d4ba619c15c76f
Miusly97 <nikita17022007@mail.ru>
2026-09-26 23:55:07 +0300

    6 номер

---
+++ seminar2_function_and_type/task-06.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+long long memo [1000];
+int trib(int n)
+{	if (n == 0)
+		return 0;
+	if (n == 1)	
+		 return 0;
+	if (n == 2)
+		return 1;
+	if (memo[n] != 0)
+		return memo[n];
+	memo[n] = trib(n-3) + trib(n-2) + trib(n-1);
+	return memo[n];
+}
+int main()
+{	int n;
+	scanf("%i", &n);
+	printf ("%i", trib(n));
+}
